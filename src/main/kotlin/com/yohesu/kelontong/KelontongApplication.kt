d7bb91180a505b4088c2d4eff5bfaf640644ea13
Yosafat Hermawan S <yosafat.h.s@gmail.com>
2026-08-11 16:41:48 +0700

    first commit

---
+++ src/main/kotlin/com/yohesu/kelontong/KelontongApplication.kt
@@ -0,0 +1,11 @@
+package com.yohesu.kelontong
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KelontongApplication
+
+fun main(args: Array<String>) {
+	runApplication<KelontongApplication>(*args)
+}
